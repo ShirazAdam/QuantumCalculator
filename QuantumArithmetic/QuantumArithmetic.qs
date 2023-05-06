2476ca4678729f42caa59abd414d560d922ad462
Shiraz Adam <93381720+ShirazAdam@users.noreply.github.com>
2023-05-06 11:10:23 +0100

    Iniitial commit of using a Q# library hosted by a C# console software

---
+++ QuantumArithmetic/QuantumArithmetic.qs
@@ -0,0 +1,36 @@
+﻿namespace QuantumArithmetic {
+
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+
+
+    operation HelloQ () : Unit
+    {
+        Message("Hello quantum world!");
+    }
+
+    operation Add(x : Int, y : Int) : Int
+    {
+        return x + y;
+    }
+
+    operation Subtract(x : Int, y : Int) : Int
+    {
+        return x - y;
+    }
+
+    operation Multiply(x : Int, y : Int) : Int
+    {
+        return x * y;
+    }
+
+    operation Divide(x : Int, y : Int) : Int
+    {
+        if (y == 0)
+        {
+            fail "Divide by zero";
+        }
+
+        return x / y;
+    }
+}
---
+++ QuantumHost/Programme.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Quantum.Simulation.Simulators;
+using QuantumArithmetic;
+
+Console.WriteLine("Hello, World!");
+using var quantumSimulator = new QuantumSimulator();
+{
+    // Add two numbers
+    var plus = await quantumSimulator.Run<Add, (long, long), long>((3, 4));
+    Console.WriteLine($"3 + 4 = {plus}");
+
+    // Subtract two numbers
+    var minus = await quantumSimulator.Run<Subtract, (long, long), long>((10, 7));
+    Console.WriteLine($"10 - 7 = {minus}");
+
+    // Multiply two numbers
+    var product = await quantumSimulator.Run<Multiply, (long, long), long>((5, 6));
+    Console.WriteLine($"5 * 6 = {product}");
+
+    // Divide two numbers
+    var shareBetween = await quantumSimulator.Run<Divide, (long, long), long>((12, 3));
+    Console.WriteLine($"12 / 3 = {shareBetween}");
+}
